535e0a269c6ee38100afdf4e1ea7107d3cc81601
eemrds <martinerik@protonmail.com>
2024-04-24 15:53:37 +0200

    testing

--- src/training/predict.py
+++ src/training/predict.py
@@ -73,7 +73,7 @@ if __name__ == "__main__":
     #     ("es", es_dataset_path, es_model_path),
     # ]
     datasets = [
-        ("all", all_data_path, full_model_path),
+        ("all", all_data_path, en_model_path),
     ]
     for lang, dataset, model in datasets:
         predict(model, dataset, lang)
